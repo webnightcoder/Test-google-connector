f89faac41859a3bc1801a727cf6f847001de0b59
bajarang-agarwal <bajarang@lucentasolutions.com>
2019-02-26 11:47:37 +0530

    initial commit

---
+++ src/Code.js
@@ -0,0 +1,24 @@
+function myFunction() {
+  
+}
+
+
+function getAuthType() {
+    return {
+        type: "NONE"
+    };
+}
+
+function getConfig() {
+    return {
+        dateRangeRequired: true
+    };
+}
+
+function getData(){
+    return "Test User";
+}
+
+function isAdminUser(){
+    return true;
+}
